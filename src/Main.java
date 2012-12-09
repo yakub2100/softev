4d6685ade68554d80fa16c043c8269e07fa41b3a
yakub2100 <yakub2101@gmail.com>
2012-12-09 01:20:01 +0000

    Created multiple choica and matching tabs

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import javax.swing.JFrame;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JEditorPane;
 import javax.swing.JTabbedPane;
+
+import java.awt.Color;
 import java.awt.Panel;
 
 import javax.swing.ButtonGroup;
@@ -17,16 +19,27 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
 import javax.swing.JRadioButton;
-import java.awt.Button;
 
 
 public class Main extends JFrame{
 	private JTextField titleText;
+	private JTextField titleTextMC;
 	private JTextArea tfText;
+	private JTextArea mcText;
+	private int row =4,matchRow=2, matchRowlbl=1;
+	private JPanel multipleChoice;
+	private JPanel matching;
+	private ArrayList<JTextField> multiFields1;
+	private ArrayList<JTextField> multiFields2;
+	private ArrayList<JTextField> matchFields1;
+	private ArrayList<JTextField> matchFields2;
+	private JTextField matchTitle;
+	
 	public Main() {
 		//setting up main frame
 		super("GIFT Editor 2000");
@@ -44,6 +57,7 @@ public class Main extends JFrame{
 		Panel trueFalse = new Panel();
 		tabbedPane.addTab("True-False", null, trueFalse, null);
 		trueFalse.setLayout(new MigLayout("", "[][grow][]", "[][186.00][][][][][][][][][]"));
+		Color bgr = trueFalse.getBackground();
 		
 		JLabel lblQuestionTitle = new JLabel("Question Title (Optional)");
 		trueFalse.add(lblQuestionTitle, "cell 0 0,alignx trailing");
@@ -70,8 +84,8 @@ public class Main extends JFrame{
 		ButtonGroup rb1 = new ButtonGroup();		
 		JRadioButton rdbtnTrue = new JRadioButton("True");	
 		JRadioButton rdbtnFalse = new JRadioButton("False");
-		rdbtnFalse.setBackground(trueFalse.getBackground());
-		rdbtnTrue.setBackground(trueFalse.getBackground());
+		rdbtnFalse.setBackground(bgr);
+		rdbtnTrue.setBackground(bgr);
 		rb1.add(rdbtnFalse);
 		rb1.add(rdbtnTrue);
 		trueFalse.add(rdbtnTrue, "flowx,cell 1 3");
@@ -89,13 +103,110 @@ public class Main extends JFrame{
 			}
 		});
 		
-		Panel matching = new Panel();
+		matching = new JPanel();
+		matching.setBackground(bgr);
 		tabbedPane.addTab("Matching", null, matching, null);
-		matching.setLayout(new MigLayout("", "[]", "[]"));
+		matching.setLayout(new MigLayout("", "[][20.00][grow][20.00][grow][]", "[][][][][][][][][][][][]"));
 		
-		JPanel multipleChoice = new JPanel();
+		JLabel lblQuestionTitleoptional = new JLabel("Question Title (Optional)");
+		matching.add(lblQuestionTitleoptional, "cell 0 0,alignx trailing");
+		
+		matchTitle = new JTextField();
+		matching.add(matchTitle, "cell 1 0,growx,spanx 6");
+		matchTitle.setColumns(10);
+		
+		JButton btnAddQa = new JButton("Add Q&A");
+		matching.add(btnAddQa, "cell 0 2,alignx center");
+		
+		JButton btnSave_2 = new JButton("Save");
+		matching.add(btnSave_2, "cell 0 "+matchRow+2+",alignx center");
+		
+		JButton btnAddAnotherMatching = new JButton("Add Another Matching Question");
+		matching.add(btnAddAnotherMatching, "cell 2 "+matchRow+5+",alignx center");
+		//add questions action
+		btnAddQa.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){	
+				matchFields1 = new ArrayList<JTextField>();
+				matchFields2 = new ArrayList<JTextField>();
+				
+				JLabel label = new JLabel(matchRowlbl+"");
+				matching.add(label, "cell 1 "+matchRow+",alignx trailing");
+				
+				JTextField field1 = new JTextField();
+				matching.add(field1, "cell 2 "+matchRow+",growx");
+				field1.setColumns(10);
+				int t = matchRowlbl+1;
+				JLabel label_1 = new JLabel(t+"");
+				matching.add(label_1, "cell 3 "+matchRow+",alignx trailing");
+				
+				JTextField field2 = new JTextField();
+				matching.add(field2, "cell 4 "+matchRow+",growx");
+				field2.setColumns(10);
+						
+				repaint();
+				matchRow++;
+				matchRowlbl+=2;
+				matchFields1.add(field1);
+				matchFields2.add(field2);
+			}
+		});
+				
+		multipleChoice = new JPanel();
 		tabbedPane.addTab("Multiple Choice", null, multipleChoice, null);
-		multipleChoice.setLayout(new MigLayout("", "[]", "[]"));
+		multipleChoice.setLayout(new MigLayout("", "[][805.00,grow][grow]", "[][186.00][][][][][][][][][]"));
+		multipleChoice.setBackground(bgr);
+		JLabel lblQuestionTitleMC = new JLabel("Question Title (Optional)");
+		multipleChoice.add(lblQuestionTitleMC, "cell 0 0,alignx trailing");
+		titleTextMC = new JTextField();
+		multipleChoice.add(titleTextMC, "cell 1 0,growx,aligny top,span 3");
+		titleTextMC.setColumns(10);
+		JLabel lblQuestionMC = new JLabel("Question");
+		lblQuestionMC.setHorizontalAlignment(SwingConstants.RIGHT);
+		multipleChoice.add(lblQuestionMC, "cell 0 1,alignx right,aligny top");
+		
+		mcText =new JTextArea (5,20);
+		
+		JScrollPane scrollPaneMC = new JScrollPane(mcText);
+		multipleChoice.add(scrollPaneMC, "cell 1 1,span 3,grow");
+		
+		JButton btnChancelclearQuestionText = new JButton("Chancel/Clear Question Text");
+		multipleChoice.add(btnChancelclearQuestionText, "cell 1 2 1 2,alignx right,spanx 2");
+		
+		JButton btnAddAnswer = new JButton("Add Answer");
+		multipleChoice.add(btnAddAnswer, "cell 0 4,alignx center");
+		
+		
+		
+		JButton btnSave_1 = new JButton("Save");
+		multipleChoice.add(btnSave_1, "cell 0 "+row+2+",alignx center");
+		
+		JButton btnAddAnotherMultiple = new JButton("Add Another Multiple Choice Question");
+		multipleChoice.add(btnAddAnotherMultiple, "cell 1 "+row+5+",alignx center");
+		//clear text action
+		btnChancelclearQuestionText.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){	
+				mcText.setText("");		
+			}
+		});
+		
+		btnAddAnswer.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){	
+				multiFields1 = new ArrayList<JTextField>();
+				multiFields2 = new ArrayList<JTextField>();
+				JTextField field1 = new JTextField();
+				multipleChoice.add(field1, "cell 1 "+row+",grow");
+				field1.setColumns(10);
+				
+				JTextField field2 = new JTextField();
+				multipleChoice.add(field2, "cell 2 "+row+",grow");
+				field2.setColumns(10);
+				repaint();
+				row++;
+				multiFields1.add(field1);
+				multiFields2.add(field2);
+			}
+		});
+		
 		
 		JPanel shortAns = new JPanel();
 		tabbedPane.addTab("Short Answer", null, shortAns, null);
